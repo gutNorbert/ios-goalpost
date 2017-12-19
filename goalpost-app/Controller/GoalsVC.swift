70c844fda448383b9ee9ae6b6d0ecf20f022b53f
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-19 13:19:41 +0100

    goalsVC

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,27 @@
+//
+//  goalpost-app
+//
+//  Created by Gutpinter Norbert on 2017. 12. 19..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+//Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+
+}
+
