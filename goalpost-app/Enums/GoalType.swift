1b8450487259a649c54dbf6df9dffb077d9d3d21
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-19 14:37:55 +0100

    static cells

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Gutpinter Norbert on 2017. 12. 19..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
